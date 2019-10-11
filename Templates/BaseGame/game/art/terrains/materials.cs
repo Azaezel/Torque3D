5d7a2b43c828f9e94d074356d30ab9b64069832d
AzaezelX <quillus@hotmail.com>
2019-10-11 14:08:19 -0500

    test of what occurs when creating a new terrain in the 4.0 preview fork as of 11 OCt 2019

---
+++ Templates/BaseGame/game/art/terrains/materials.cs
@@ -0,0 +1,8 @@
+
+new TerrainMaterial()
+{
+   normalMap = "data/pbr/images/greasy-pan-2-normal";
+   internalName = "woods";
+   detailMap = "data/pbr/images/greasy-pan-2-albedo";
+   diffuseMap = "data/pbr/images/rustediron_albedo";
+};
---
+++ Templates/BaseGame/game/data/pbr/shapes/artteam_tools/materials.cs
@@ -0,0 +1,9 @@
+
+new Material(mcolors_tools)
+{
+   emissive[0] = true;
+   glow[0] = true;
+   mapTo = "colors_tools";
+   diffuseMap[0] = "data/pbr/shapes/artteam_tools/colors_tools";
+   castDynamicShadows = "1";
+};
