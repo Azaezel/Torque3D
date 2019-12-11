ab37148647d80a5ca6acac1bbd76bf253d4ed228
AzaezelX <quillus@hotmail.com>
2019-12-11 01:58:05 -0600

    yet more arithmetic overflow warn cleanups

--- Engine/source/core/color.h
+++ Engine/source/core/color.h
@@ -843,15 +843,15 @@ inline ColorI LinearColorF::toColorI(const bool keepAsLinear)
       else
       {
    #ifdef TORQUE_USE_LEGACY_GAMMA
-         float r = mPow(red, gOneOverGamma);
-         float g = mPow(green, gOneOverGamma);
-         float b = mPow(blue, gOneOverGamma);
+         F32 r = mPow(red, gOneOverGamma);
+         F32 g = mPow(green, gOneOverGamma);
+         F32 b = mPow(blue, gOneOverGamma);
          return ColorI(U8(r * 255.0f + 0.5), U8(g * 255.0f + 0.5), U8(b * 255.0f + 0.5), U8(alpha * 255.0f + 0.5));
    #else
-         float r = red < 0.0031308f ? 12.92f * red : 1.055 * mPow(red, 1.0f / 2.4f) - 0.055f;
-         float g = green < 0.0031308f ? 12.92f * green : 1.055 * mPow(green, 1.0f / 2.4f) - 0.055f;
-         float b = blue < 0.0031308f ? 12.92f * blue : 1.055 * mPow(blue, 1.0f / 2.4f) - 0.055f;
-         return ColorI(U8(r * 255.0f + 0.5), U8(g * 255.0f + 0.5), U8(b * 255.0f + 0.5), U8(alpha * 255.0f + 0.5));
+         F32 r = red < 0.0031308f ? 12.92f * red : 1.055f * mPow(red, 1.0f / 2.4f) - 0.055f;
+         F32 g = green < 0.0031308f ? 12.92f * green : 1.055f * mPow(green, 1.0f / 2.4f) - 0.055f;
+         F32 b = blue < 0.0031308f ? 12.92f * blue : 1.055f * mPow(blue, 1.0f / 2.4f) - 0.055f;
+         return ColorI(U8(r * 255.0f + 0.5), U8(g * 255.0f + 0.5f), U8(b * 255.0f + 0.5f), U8(alpha * 255.0f + 0.5f));
    #endif
       }
    }
@@ -867,15 +867,15 @@ inline ColorI LinearColorF::toColorI(const bool keepAsLinear)
       else
       {
    #ifdef TORQUE_USE_LEGACY_GAMMA
-         float r = mPow(red, gOneOverGamma);
-         float g = mPow(green, gOneOverGamma);
-         float b = mPow(blue, gOneOverGamma);
+         F32 r = mPow(red, gOneOverGamma);
+         F32 g = mPow(green, gOneOverGamma);
+         F32 b = mPow(blue, gOneOverGamma);
          return ColorI(U8(r * 255.0f + 0.5), U8(g * 255.0f + 0.5), U8(b * 255.0f + 0.5), U8(alpha * 255.0f + 0.5));
    #else
-         float r = red < 0.0031308f ? 12.92f * red : 1.055 * mPow(red, 1.0f / 2.4f) - 0.055f;
-         float g = green < 0.0031308f ? 12.92f * green : 1.055 * mPow(green, 1.0f / 2.4f) - 0.055f;
-         float b = blue < 0.0031308f ? 12.92f * blue : 1.055 * mPow(blue, 1.0f / 2.4f) - 0.055f;
-         return ColorI(U8(r * 255.0f + 0.5), U8(g * 255.0f + 0.5), U8(b * 255.0f + 0.5), U8(alpha * 255.0f + 0.5));
+         F32 r = red < 0.0031308f ? 12.92f * red : 1.055f * mPow(red, 1.0f / 2.4f) - 0.055f;
+         F32 g = green < 0.0031308f ? 12.92f * green : 1.055f * mPow(green, 1.0f / 2.4f) - 0.055f;
+         F32 b = blue < 0.0031308f ? 12.92f * blue : 1.055f * mPow(blue, 1.0f / 2.4f) - 0.055f;
+         return ColorI(U8(r * 255.0f + 0.5), U8(g * 255.0f + 0.5f), U8(b * 255.0f + 0.5f), U8(alpha * 255.0f + 0.5f));
    #endif
       }
    }
